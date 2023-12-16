9d32742979476fc70f054903d88e40a57508f2ef
Omkar Sharma <omkarsharma2821@gmail.com>
2023-12-16 21:19:04 +0530

    added component and router

---
+++ backend/models/AdminModel.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('../connection');
+
+const myschema= new Schema({
+
+    email:{ type:String, require:true},
+    name: String,
+    password: String,
+    avatar:{type:String, default: 'avatar_placeholder.png'},
+    createdAt: Date
+});
+module.exports=model('admin', myschema);
